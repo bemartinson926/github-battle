574e5cb51f6630ba77ac45b31dab5c71999b4720
Blake Martinson <blake.martinson@gmail.com>
2016-06-29 22:09:08 -0400

    Add props

--- app/index.js
+++ app/index.js
@@ -1,15 +1,16 @@
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div>Hello World</div>
+      <div>Hello {this.props.name}</div>
     )
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <HelloWorld name="Blake" />,
   document.getElementById('app')
 );
