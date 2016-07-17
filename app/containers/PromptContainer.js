1d29f9d2a2a261232789579aca13b062f83ae322
Blake Martinson <blake.martinson@gmail.com>
2016-07-17 17:13:46 -0400

    Add state to prompt component; add form submission

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -2,18 +2,56 @@ var React = require('react');
 var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
-  render: function () {
-    console.log(this);
+  contextTypes: {
+    // contextTypes makes it so you dont have to pass the router down as props
+    // Allows us to pass items to components wo going through props
+    router: React.PropTypes.object.isRequired
+  },
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+  onUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    })
+  },
+  onSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
 
+    if (this.props.routeParams.playerOne) {
+      console.log('if context', this.context);
+
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      console.log('else context', this.context);
+
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+  render: function () {
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         <h1>{this.props.route.header}</h1>
         <div className="col-sm-12">
-          <form>
+          <form onSubmit={this.onSubmitUser}>
             <div className="form-group">
               <input
                 className="form-control"
                 placeholer="Github Username"
+                onChange={this.onUpdateUser}
+                value={this.state.username}
                 type="text" />
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
