d7da201ecc8e058a43f43415700043c8cacc83a9
Blake Martinson <blake.martinson@gmail.com>
2016-07-30 18:02:20 -0400

    Add propTypes to ConfirmBattle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,7 +1,16 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
 
 function ConfirmBattle (props) {
-  return props.isLoading === true ? <p>Loading!</p> : <p>Confirm Battle!</p>
+  return props.isLoading === true
+    ? <p>Loading!</p>
+    : <p>Confirm Battle!</p>
+}
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired
 }
 
 module.exports = ConfirmBattle;
